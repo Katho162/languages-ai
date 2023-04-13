59c9453303329a05d6d64df7b413969da01f5cc5
BuildTools <unconfigured@null.spigotmc.org>
2023-04-13 00:06:31 -0300

    feat: Helm Ingress

--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
 # Create kind cluster
 kind create cluster --config=./kind-config.yaml --name languages-ai
 
+helm uninstall languages-ai
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/kind/deploy.yaml
+
 cd web
 
 # Build images and load to the cluster
 docker build -t web:1.0 . && kind load docker-image web:1.0 --name languages-ai
+
+cd .. && cd user-auth-api
+
+docker build -t user-auth:1.0 . && kind load docker-image user-auth:1.0 --name languages-ai
 
 cd ..
 
