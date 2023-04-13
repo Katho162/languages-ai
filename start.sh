8188da07d0c14ee1028808deab5cd4e7377deea7
BuildTools <unconfigured@null.spigotmc.org>
2023-04-13 04:05:41 -0300

    feat: k8s Ingress

--- start.sh
+++ start.sh
@@ -3,16 +3,21 @@ kind create cluster --config=./kind-config.yaml --name languages-ai
 
 helm uninstall languages-ai
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/kind/deploy.yaml
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
 
 cd web
 
 # Build images and load to the cluster
 docker build -t web:1.0 . && kind load docker-image web:1.0 --name languages-ai
 
-cd .. && cd user-auth-api
+# cd .. && cd user-auth-api
 
-docker build -t user-auth:1.0 . && kind load docker-image user-auth:1.0 --name languages-ai
+# docker build -t user-auth:1.0 . && kind load docker-image user-auth:1.0 --name languages-ai
 
 cd ..
 
