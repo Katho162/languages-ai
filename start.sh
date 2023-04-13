1ffa85f83e4d0cc4d28b320642750f38b3197f8d
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 23:27:30 -0300

    feat: Helm files

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ docker build -t web:1.0 . && kind load docker-image web:1.0 --name languages-ai
 cd ..
 
 # Apply the deployment and services config
-kubectl apply -f ./manifest.yaml
+helm install languages-ai ./languages-ai
